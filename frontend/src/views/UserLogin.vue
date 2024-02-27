dbea3ccb0e4bea6bddaffd1081f2196279cb206a
teloryfrozy <raptor.frozy@gmail.com>
2024-02-27 02:20:05 +0200

    Add Hide/Show password button in UserLogin

--- frontend/src/views/UserLogin.vue
+++ frontend/src/views/UserLogin.vue
@@ -25,11 +25,39 @@
                             <a href="#" class="font-semibold text-gray-900 hover:text-gray-600">Mot de passe oublié ?</a>
                         </div>
                     </div>
-                    <div class="mt-2">
+                    <!-- <div class="mt-2">
                         <input id="password" v-model="password" name="password" type="password"
                             autocomplete="current-password" required
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6">
-                    </div>
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"> -->
+
+
+
+                        <div class="relative items-stretch mt-2 flex">
+                            <input v-if="!showPassword" type="password"
+                                class="flex-1 rounded-l-md border-0 pl-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-800 sm:text-sm sm:leading-6"
+                                v-model="password" />
+                            <input v-else type="text"
+                                class="flex-1 rounded-l-md border-0 pl-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-800 sm:text-sm sm:leading-6"
+                                v-model="password" />
+                            <div class="flex items-center">
+                                <button @click.prevent="togglePasswordVisibility"
+                                    class="p-2 bg-gray-50 rounded-r-md ring-l-none ring-1 ring-inset ring-gray-300">
+                                    <svg v-if="!showPassword" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                    </svg>
+                                    <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    <!-- </div> -->
                 </div>
 
                 <div>
@@ -58,6 +86,7 @@ import { API_BASE_URL } from '@/main';
 const router = useRouter();
 let username = ref('');
 let password = ref('');
+let showPassword = ref(false);
 // Variables to display a notification
 let showNotification = ref(false);
 let notificationTitle = ref('');
@@ -66,13 +95,15 @@ let backgroundColor = ref('');
 
 
 onMounted(() => {
-
     // Run the function every second
     setInterval(() => {
         showNotification.value = false;
     }, 1000);
 });
 
+function togglePasswordVisibility() {
+    showPassword.value = !showPassword.value;
+}
 
 // Function to handle login
 async function login() {

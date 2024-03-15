45f1b30e77094e07da813b6e85839344daf0a67c
teloryfrozy <raptor.frozy@gmail.com>
2024-03-15 21:40:09 +0000

    Add Google icon in Signup part 2

--- frontend/src/views/SignUpV1_part2.vue
+++ frontend/src/views/SignUpV1_part2.vue
@@ -367,8 +367,27 @@
                         </div>
                       </div>
                       <div class="py-6">
-                        <div class="relative items-stretch mt-2">
-                          <button @click="authorize_google">Lier votre compte Google</button>
+                        <div class="relative items-stretch mt-2 flex justify-center items-center">
+                          <button type="button"
+                            class="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            @click="authorize_google">
+                            <svg class="-ml-0.5 h-5 w-5" aria-hidden="true" viewBox="0 0 24 24"
+                              xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+                              <path
+                                d="M23.4392061,12.2245191 C23.4392061,11.2412519 23.3594198,10.5237252 23.1867481,9.77963359 L11.9587786,9.77963359 L11.9587786,14.2176183 L18.5493435,14.2176183 C18.4165191,15.3205191 17.6989924,16.9814656 16.104458,18.0975573 L16.0821069,18.2461374 L19.6321832,20.9963359 L19.8781374,21.0208855 C22.1369771,18.9347176 23.4392061,15.8652824 23.4392061,12.2245191"
+                                id="Shape" fill="#4285F4"></path>
+                              <path
+                                d="M11.9587786,23.9175573 C15.1876031,23.9175573 17.898229,22.8545038 19.8781374,21.0208855 L16.104458,18.0975573 C15.094626,18.8018015 13.7392672,19.2934351 11.9587786,19.2934351 C8.79636641,19.2934351 6.11230534,17.2073588 5.15551145,14.3239695 L5.01526718,14.3358779 L1.32384733,17.1927023 L1.27557252,17.3269008 C3.24210687,21.2334046 7.28152672,23.9175573 11.9587786,23.9175573"
+                                id="Shape" fill="#34A853"></path>
+                              <path
+                                d="M5.15551145,14.3239695 C4.90305344,13.5798779 4.75694656,12.7825649 4.75694656,11.9587786 C4.75694656,11.1349008 4.90305344,10.3376794 5.14222901,9.59358779 L5.13554198,9.4351145 L1.3978626,6.53239695 L1.27557252,6.59056489 C0.465068702,8.21166412 0,10.0320916 0,11.9587786 C0,13.8854656 0.465068702,15.7058015 1.27557252,17.3269008 L5.15551145,14.3239695"
+                                id="Shape" fill="#FBBC05"></path>
+                              <path
+                                d="M11.9587786,4.62403053 C14.2043359,4.62403053 15.719084,5.59401527 16.5828092,6.40461069 L19.9578321,3.10928244 C17.8850382,1.18259542 15.1876031,0 11.9587786,0 C7.28152672,0 3.24210687,2.68406107 1.27557252,6.59056489 L5.14222901,9.59358779 C6.11230534,6.71019847 8.79636641,4.62403053 11.9587786,4.62403053"
+                                id="Shape" fill="#EB4335"></path>
+                            </svg>
+                            Lier mon compte Google
+                          </button>
                         </div>
                       </div>
                       <div class="relative">
@@ -381,7 +400,7 @@
                       </div>
                       <div class="pt-4">
                         <div class="relative items-stretch mt-2">
-                          <button @click="authorize_microsoft">Lier votre compte Outlook</button>
+                          <button @click="authorize_microsoft">Lier mon compte Outlook</button>
                         </div>
                       </div>
                       <div>

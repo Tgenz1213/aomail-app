9a6f083717b1dd01b4a41f40ee5d0dc262e47df5
Theo <theo@vps-feaf3d65.vps.ovh.net>
2024-03-15 22:26:19 +0000

    Updated design Signup Buttons

--- frontend/src/views/SignUpV1_part2.vue
+++ frontend/src/views/SignUpV1_part2.vue
@@ -366,10 +366,10 @@
                           <span class="bg-white px-2 text-sm text-gray-500">Lier un compte Gmail</span>
                         </div>
                       </div>
-                      <div class="py-6">
+                      <div class="py-4">
                         <div class="relative items-stretch mt-2 flex justify-center items-center">
                           <button type="button"
-                            class="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            class="inline-flex items-center gap-x-2 rounded-md bg-gray-700 px-3 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             @click="authorize_google">
                             <svg class="-ml-0.5 h-5 w-5" aria-hidden="true" viewBox="0 0 24 24"
                               xmlns="http://www.w3.org/2000/svg" fill="currentColor">
@@ -401,7 +401,7 @@
                       <div class="pt-4">
                         <div class="relative items-stretch mt-2 flex justify-center items-center">
                           <button type="button"
-                            class="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            class="inline-flex items-center gap-x-2 rounded-md bg-gray-700 px-3 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             @click="authorize_microsoft">
                             <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21">
                               <title>MS-SymbolLockup</title>
@@ -415,7 +415,7 @@
                         </div>
                       </div>
                       <div>
-                        <div class="pt-8">
+                        <div class="pt-10">
                           <button @click="nextStep3"
                             class="flex w-full justify-center rounded-md bg-gray-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-800">Continuer</button>
                         </div>

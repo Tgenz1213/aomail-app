dc10567fa338ece40758e609a174abd8ea5963a9
teloryfrozy <raptor.frozy@gmail.com>
2024-03-17 16:51:23 +0000

    Improve script for functions that have 2 or 3 parameters

--- backend/MailAssistant/_test_ai_script.py
+++ backend/MailAssistant/_test_ai_script.py
@@ -8,11 +8,27 @@ import gpt_3_5_turbo, claude, mistral
 # TODO: add gpt_4
 # TODO: add role as a parameter in get_prompt_response for testing by role
 # TODO: generate a pdf file
-LIST_AI_PROVIDERS = [gpt_3_5_turbo, claude, mistral]
 
+# TODO: add a counter per ia in the try except to check the success rate
+# TODO: store errors for debugging
+LIST_AI_PROVIDERS = [gpt_3_5_turbo, claude, mistral]
+LIST_FUNCTIONS = ["new_mail_recommendation", "correct_mail_language_mistakes"]
+
+# Créer une liste combinée des fournisseurs AI et des fonctions
+COMBINED_LIST = [
+    (provider, function)
+    for provider in LIST_AI_PROVIDERS
+    for function in LIST_FUNCTIONS
+]
+
+# Initialiser le dictionnaire pour stocker les statistiques
+ai_providers_stats = {
+    provider.__name__: {function: [] for _, function in COMBINED_LIST}
+    for provider, _ in COMBINED_LIST
+}
 
 # ----------------------- new_mail_recommendation EXAMPLES -----------------------#
-examples = [
+examples_new_mail_recommendation = [
     (
         "<p>Hello, can I ask a question about Graph API implementation inside an app here?</p>",
         "",
@@ -50,41 +66,81 @@ examples = [
     ),
 ]
 
+# ----------------------- new_correct_mail_language_mistakes EXAMPLES -----------------------#
+examples_correct_mail_language_mistakes = [
+    ("Salut, cmt ça va?", "Juste pr prendre de tes nouvelles, cmt ça va?"),
+    ("Lol, tu là?", "Hey, lol, tu là?"),
+    ("A tooute à l'h", "A laproch  on se voit plus tard!"),
+    ("peti test appl", "c un  emai de test envoyer depus l'appl"),
+    ("Hey! Quoi de neuf?", "Hey! Quoi de neuf? Pas de news de toî depuis un moment."),
+    ("Attend ta réponse", "Toujours en atente de ta réponse o dernier mail."),
+    ("Merci", "Merci pr l'info, c'est simpà!"),
+    ("Coucou", "Coucou, t'as une minute? Besoin dton avis sur kke chose."),
+    (
+        "Discus proj entrprena",
+        "Bonjour, j'espère que tu va bi1. Je voula discuter avec toi du proj",
+    ),
+]
 
+
+
+
+
+# Dictionnaire pour stocker les résultats
 results = {}
-ai_providers_stats = {provider.__name__: [] for provider in LIST_AI_PROVIDERS}
 
-for example in examples:
-    mail_content, email_subject, user_recommendation = example
+# Parcourir les exemples
+for example in (
+    examples_new_mail_recommendation + examples_correct_mail_language_mistakes
+):
+    # Déterminer le nombre d'éléments par tuple
+    num_elements = len(example)
 
+    # Assigner les éléments du tuple
+    if num_elements == 3:
+        mail_content, email_subject, user_recommendation = example
+    elif num_elements == 2:
+        mail_content, email_subject = example
+    else:
+        continue
+
+    # Parcourir les fournisseurs AI
     for ai_provider in LIST_AI_PROVIDERS:
-        start_time = time.time()
-        # TODO: add a counter per ia to check the success rate
-        # TODO: store errors for debugging
-        try:
-            result = ai_provider.new_mail_recommendation(
-                mail_content, email_subject, user_recommendation
-            )
-        except:
-            pass
-        elapsed_time = time.time() - start_time
+        # Parcourir les fonctions AI
+        for function in LIST_FUNCTIONS:
+            start_time = time.time()
+            try:
+                # Appeler la fonction AI correspondante
+                if num_elements == 3:
+                    result = getattr(ai_provider, function)(
+                        mail_content, email_subject, user_recommendation
+                    )
+                elif num_elements == 2:
+                    result = getattr(ai_provider, function)(mail_content, email_subject)
 
-        # Store the results
-        results[(mail_content, email_subject, ai_provider.__name__)] = {
-            "result": result,
-            "elapsed_time": elapsed_time,
-            "ai_provider": ai_provider.__name__,
-        }
-        ai_providers_stats[ai_provider.__name__].append(elapsed_time)
+            except:
+                pass
+            elapsed_time = time.time() - start_time
 
-        print(f"AI Provider: {ai_provider.__name__}")
-        print(f"{Fore.LIGHTBLUE_EX}{result}")
-        print(f"Elapsed Time: {elapsed_time} seconds\n")
+            # Stocker les résultats
+            results[(mail_content, email_subject, ai_provider.__name__, function)] = {
+                "result": result,
+                "elapsed_time": elapsed_time,
+                "ai_provider": ai_provider.__name__,
+                "function_name": function,
+                "example": example,
+            }
+            ai_providers_stats[ai_provider.__name__][function].append(elapsed_time)
 
-# Extracting elapsed times for all prompts
+            # Afficher les résultats
+            print(f"AI Provider: {ai_provider.__name__}, Function: {function}")
+            print(f"{Fore.LIGHTBLUE_EX}{result}")
+            print(f"Elapsed Time: {elapsed_time} seconds\n")
+
+# Extraire les temps écoulés pour tous les prompts
 all_elapsed_times = [result["elapsed_time"] for result in results.values()]
 
-# ----------------------- PRINT RESULTS -----------------------#
+# Afficher les statistiques générales
 min_time = round(min(all_elapsed_times), 2)
 max_time = round(max(all_elapsed_times), 2)
 print(f"Min Time: {min_time} seconds")
@@ -96,22 +152,17 @@ print(f"Avg Time for All Prompts: {avg_time_all_prompts} seconds")
 std_dev_all_prompts = round(statistics.stdev(all_elapsed_times), 2)
 print(f"Standard Deviation for All Prompts: {std_dev_all_prompts} seconds")
 
-# Print statistics for each AI provider
-for provider, elapsed_times in ai_providers_stats.items():
-    min_time_provider = round(min(elapsed_times), 2)
-    max_time_provider = round(max(elapsed_times), 2)
-    avg_time_provider = round(statistics.mean(elapsed_times), 2)
-    std_dev_provider = round(statistics.stdev(elapsed_times), 2)
-
+# Afficher les statistiques pour chaque fournisseur AI et fonction
+for provider, functions in ai_providers_stats.items():
     print(f"\nAI Provider: {provider}")
-    print(f"Min Time: {min_time_provider} seconds")
-    print(f"Max Time: {max_time_provider} seconds")
-    print(f"Avg Time: {avg_time_provider} seconds")
-    print(f"Standard Deviation: {std_dev_provider} seconds")
+    for function, elapsed_times in functions.items():
+        min_time_function = round(min(elapsed_times), 2)
+        max_time_function = round(max(elapsed_times), 2)
+        avg_time_function = round(statistics.mean(elapsed_times), 2)
+        std_dev_function = round(statistics.stdev(elapsed_times), 2)
 
-
-"""    start_time = time.time()
-correct_mail_language_mistakes("peti test appl", "c un  emai de test envoyer depus l'appl")
-execution_time = time.time() - start_time
-
-print(f"{Fore.GREEN}Le temps d'exécution du script n°2 est de {execution_time} secondes.")"""
+        print(f"\tFunction: {function}")
+        print(f"\tMin Time: {min_time_function} seconds")
+        print(f"\tMax Time: {max_time_function} seconds")
+        print(f"\tAvg Time: {avg_time_function} seconds")
+        print(f"\tStandard Deviation: {std_dev_function} seconds")

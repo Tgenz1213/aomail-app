c7197ea32f83891c0419d955dcae3a6367d52c52
teloryfrozy <raptor.frozy@gmail.com>
2024-03-23 11:04:17 +0000

    Fix missing imports when merging

--- backend/MailAssistant/email_providers/google_api.py
+++ backend/MailAssistant/email_providers/google_api.py
@@ -34,7 +34,7 @@ from MailAssistant.constants import (
     GOOGLE_SCOPES,
 )
 from .. import library
-from ..models import SocialAPI, Contact, BulletPoint, Category, Email, Sender
+from ..models import Rule, SocialAPI, Contact, BulletPoint, Category, Email, Sender
 from base64 import urlsafe_b64encode
 
 
@@ -732,7 +732,7 @@ def email_to_bdd(user, services, id_email):
                     decoded_data = library.format_mail(decoded_data)
 
                 # Get user categories
-                category_list = library.get_db_categories(request.user)
+                category_list = library.get_db_categories(user)
 
                 # Process the email data with AI/NLP
                 # user_description = "Enseignant chercheur au sein d'une école d'ingénieur ESAIP."
@@ -791,8 +791,8 @@ def email_to_bdd(user, services, id_email):
                         answer_later=False,
                         sender=sender,
                         category=category,
-                        user=request.user,
-                        date=date,
+                        user=user,
+                        date=sent_date,
                     )
 
                     # If the email has a summary, save it in the BulletPoint table
@@ -818,7 +818,7 @@ def email_to_bdd(user, services, id_email):
                 decoded_data = library.format_mail(decoded_data)
 
             # Get user categories
-            category_list = library.get_db_categories(request.user)
+            category_list = library.get_db_categories(user)
 
             # Process the email data with AI/NLP
             # user_description = "Enseignant chercheur au sein d'une école d'ingénieur ESAIP."
@@ -851,7 +851,7 @@ def email_to_bdd(user, services, id_email):
             sender, _ = Sender.objects.get_or_create(name=sender_name, email=sender_email)
 
             # Get the relevant category based
-            category = Category.objects.get_or_create(name=topic, user=request.user)[0]
+            category = Category.objects.get_or_create(name=topic, user=user)[0]
 
             provider = "Gmail"
 
@@ -867,8 +867,8 @@ def email_to_bdd(user, services, id_email):
                     answer_later=False,
                     sender=sender,
                     category=category,
-                    user=request.user,
-                    date=date,
+                    user=user,
+                    date=sent_date,
                 )
 
                 # If the email has a summary, save it in the BulletPoint table

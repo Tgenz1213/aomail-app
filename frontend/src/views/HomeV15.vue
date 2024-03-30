1fc9d85c9f784836011b2db8ff4f58a441afe1dd
teloryfrozy <raptor.frozy@gmail.com>
2024-03-30 09:59:25 +0000

    Add pop up if category error is returned from backend

--- frontend/src/views/HomeV15.vue
+++ frontend/src/views/HomeV15.vue
@@ -738,7 +738,7 @@
                                                                     <!-- remove @click="toggleEmailVisibility"-->
                                                                     <p @click="toggleEmailVisibility"
                                                                         class="cursor-pointer">Vous avez reçu <span
-                                                                            class="font-semibold text-gray-900 dark:text-white hover:text-gray-700 w-full">{{ emails[selectedTopic]['Useless'].filter(email => !email.answer_later).length }}</span>
+                                                                            class="font-semibold text-gray-900 dark:text-white hover:text-gray-700 w-full">{{ emails[selectedTopic]['Useless'].filter(email=> !email.answer_later).length }}</span>
                                                                         <span
                                                                             v-if="emails[selectedTopic]['Useless'].filter(email => !email.answer_later).length === 1">
                                                                             mail inutile.
@@ -1766,9 +1766,15 @@ async function handleAddCategory(categoryData) {
             }),
         });
 
-        console.log("response", response)
-        // TODO : check if it really works => print error if there are
-        if (response) {
+        if ('error' in response) {
+            // Show the pop-up
+            backgroundColor = 'bg-red-300';
+            notificationTitle = 'Erreur lors de l\'ajout de la catégorie';
+            notificationMessage = response.error;
+            displayPopup();
+
+            closeModal();
+        } else if (response) {
             // Show the pop-up
             backgroundColor = 'bg-green-300';
             notificationTitle = 'Succès !';

90eedc395fc6e157b402d278ffa91e8ed1970804
teloryfrozy <raptor.frozy@gmail.com>
2024-04-13 07:02:22 +0000

    Add scrollable div with fake emails linked

--- frontend/src/views/SettingsV1.vue
+++ frontend/src/views/SettingsV1.vue
@@ -345,22 +345,31 @@
                                     </div>
 
 
-
-                                    <ul role="list" class="space-y-3">
-                                        <li v-for="email in emailLinked" :key="email.email"
-                                            class="flex items-center justify-between overflow-hidden font-semibold rounded-md bg-gray-50 px-6 py-4 shadow hover:shadow-md text-gray-700 relative">
-                                            <span>{{ email.email }}</span>
-                                            <button type="button"
-                                                class="inline-flex justify-center items-center gap-x-1 rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-700"
-                                                @click="unLinkAccount(email)">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                                </svg>
-                                            </button>
-                                        </li>
-                                    </ul>
+                                    <div class="overflow-y-auto" style="max-height: 120px;">
+                                        <!-- TODO: set dynamicelly -->
+                                        <div class="flex justify-center">
+                                            <div class="w-full max-w-lg">
+                                                <div class="overflow-y-auto h-40"> <!-- TODO: set dynamicelly -->
+                                                    <ul role="list" class="space-y-1">
+                                                        <li v-for="email in emailsLinked" :key="email.email"
+                                                            class="flex items-center justify-between overflow-hidden font-semibold rounded-md bg-gray-10 px-6 py-1 shadow hover:shadow-md text-gray-700 relative">
+                                                            <span>{{ email.email }}</span>
+                                                            <button type="button"
+                                                                class="inline-flex justify-center items-center gap-x-1 rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-700"
+                                                                @click="unLinkAccount(email)">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                                    viewBox="0 0 24 24" stroke-width="1.5"
+                                                                    stroke="currentColor" class="w-6 h-6">
+                                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                                </svg>
+                                                            </button>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
 
 
 
@@ -562,7 +571,7 @@ let activeSection = ref('preferences'); // Default active section
 let bgColor = ref(localStorage.getItem('bgColor') || '');
 let userData = ref('');
 let emailInput = ref('');
-let emailLinked = ref([]);
+let emailsLinked = ref([{ "email": "test", "provider": "google" }, { "email": "test2", "provider": "outlook" }, { "email": "test2", "provider": "outlook" }]);
 let newPassword = ref('');
 let confirmPassword = ref('');
 
@@ -598,7 +607,7 @@ async function fetchEmailLinked() {
 
     // try {
     //     const response = await fetchWithToken(`${API_BASE_URL}user/preferences/emails_linked/`, requestOptions);
-    //     emailLinked.value = response;
+    //     emailsLinked.value = response;
     // } catch (error) {
     //     // Show the pop-up
     //     backgroundColor = 'bg-red-300';

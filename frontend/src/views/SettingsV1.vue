684a6a24ded6780642f95aaad636a60335dc79be
teloryfrozy <raptor.frozy@gmail.com>
2024-04-13 06:50:38 +0000

    Start to refactor 'My Account' in settings for multi account handling

--- frontend/src/views/SettingsV1.vue
+++ frontend/src/views/SettingsV1.vue
@@ -207,15 +207,26 @@
                         <!-- TO DO : CENTER -->
                         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-10">
                             <!-- We've used 3xl here, but feel free to try other max-widths based on your needs -->
-                            <div class="mx-auto max-w-lg">
-                                <!-- Content goes here -->
+
+
+
+
+
+                            <div class="flex justify-between">
+
+
+
+
+
+
                                 <div class="flex flex-col">
                                     <div class="relative">
                                         <div class="absolute inset-0 flex items-center" aria-hidden="true">
                                             <div class="w-full border-t border-gray-300"></div>
                                         </div>
                                         <div class="relative flex justify-center">
-                                            <span class="bg-white px-2 text-sm text-gray-500">Changement d'identifiant
+                                            <span class="bg-white px-2 text-sm text-gray-500">Changement
+                                                d'identifiant
                                                 ou de
                                                 mot de passe</span>
                                         </div>
@@ -308,34 +319,138 @@
                                         <div class="flex justify-end pt-4">
                                             <button @click="handleSubmit"
                                                 class="rounded-md bg-gray-800 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">Modifier</button>
-                                            <!-- TO CODE : Notification system that worn the user when the notification appears -->
                                         </div>
                                     </div>
+                                </div>
+
+
+
+                                <div class="flex-col">
                                     <div class="relative">
                                         <div class="absolute inset-0 flex items-center" aria-hidden="true">
                                             <div class="w-full border-t border-gray-300"></div>
                                         </div>
                                         <div class="relative flex justify-center">
-                                            <span class="bg-white px-2 text-sm text-gray-500">Suppression du
-                                                compte</span>
+                                            <span class="bg-white px-2 text-sm text-gray-500">Lier un nouvel
+                                                email</span>
                                         </div>
                                     </div>
-                                    <div class="pt-6">
-                                        <div class="flex space-x-1 items-center">
+                                    <div class="flex space-x-1 items-center">
+                                        <envelope-icon class="w-4 h-4" />
+                                        <label class="block text-sm font-medium leading-6 text-gray-900">Email</label>
+                                    </div>
+                                    <div class="relative items-stretch mt-2">
+                                        <input v-model="emailInput" type="text"
+                                            class="block w-full rounded-md border-0 pl-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-800 sm:text-sm sm:leading-6">
+                                    </div>
+
+
+
+                                    <ul role="list" class="space-y-3">
+                                        <li v-for="email in emailLinked" :key="email.email"
+                                            class="flex items-center justify-between overflow-hidden font-semibold rounded-md bg-gray-50 px-6 py-4 shadow hover:shadow-md text-gray-700 relative">
+                                            <span>{{ email.email }}</span>
+                                            <button type="button"
+                                                class="inline-flex justify-center items-center gap-x-1 rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-700"
+                                                @click="unLinkAccount(email)">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                </svg>
+                                            </button>
+                                        </li>
+                                    </ul>
+
+
+
+
+
+
+
+
+                                    <div class="flex flex-row gap-2">
+
+                                        <div class="pt-4">
+                                            <div class="relative items-stretch mt-2 flex justify-center items-center">
+                                                <button type="button"
+                                                    class="inline-flex items-center gap-x-2 rounded-md bg-gray-700 px-3 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                                    @click="authorize_microsoft">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21"
+                                                        viewBox="0 0 21 21">
+                                                        <title>MS-SymbolLockup</title>
+                                                        <rect x="1" y="1" width="9" height="9" fill="#f25022" />
+                                                        <rect x="1" y="11" width="9" height="9" fill="#00a4ef" />
+                                                        <rect x="11" y="1" width="9" height="9" fill="#7fba00" />
+                                                        <rect x="11" y="11" width="9" height="9" fill="#ffb900" />
+                                                    </svg>
+                                                    Lier mon compte Outlook
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div class="py-4">
+                                            <div class="relative items-stretch mt-2 flex justify-center items-center">
+                                                <button type="button"
+                                                    class="inline-flex items-center gap-x-2 rounded-md bg-gray-700 px-3 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                                    @click="authorize_google">
+                                                    <svg class="-ml-0.5 h-5 w-5" aria-hidden="true" viewBox="0 0 24 24"
+                                                        xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+                                                        <path
+                                                            d="M23.4392061,12.2245191 C23.4392061,11.2412519 23.3594198,10.5237252 23.1867481,9.77963359 L11.9587786,9.77963359 L11.9587786,14.2176183 L18.5493435,14.2176183 C18.4165191,15.3205191 17.6989924,16.9814656 16.104458,18.0975573 L16.0821069,18.2461374 L19.6321832,20.9963359 L19.8781374,21.0208855 C22.1369771,18.9347176 23.4392061,15.8652824 23.4392061,12.2245191"
+                                                            id="Shape" fill="#4285F4"></path>
+                                                        <path
+                                                            d="M11.9587786,23.9175573 C15.1876031,23.9175573 17.898229,22.8545038 19.8781374,21.0208855 L16.104458,18.0975573 C15.094626,18.8018015 13.7392672,19.2934351 11.9587786,19.2934351 C8.79636641,19.2934351 6.11230534,17.2073588 5.15551145,14.3239695 L5.01526718,14.3358779 L1.32384733,17.1927023 L1.27557252,17.3269008 C3.24210687,21.2334046 7.28152672,23.9175573 11.9587786,23.9175573"
+                                                            id="Shape" fill="#34A853"></path>
+                                                        <path
+                                                            d="M5.15551145,14.3239695 C4.90305344,13.5798779 4.75694656,12.7825649 4.75694656,11.9587786 C4.75694656,11.1349008 4.90305344,10.3376794 5.14222901,9.59358779 L5.13554198,9.4351145 L1.3978626,6.53239695 L1.27557252,6.59056489 C0.465068702,8.21166412 0,10.0320916 0,11.9587786 C0,13.8854656 0.465068702,15.7058015 1.27557252,17.3269008 L5.15551145,14.3239695"
+                                                            id="Shape" fill="#FBBC05"></path>
+                                                        <path
+                                                            d="M11.9587786,4.62403053 C14.2043359,4.62403053 15.719084,5.59401527 16.5828092,6.40461069 L19.9578321,3.10928244 C17.8850382,1.18259542 15.1876031,0 11.9587786,0 C7.28152672,0 3.24210687,2.68406107 1.27557252,6.59056489 L5.14222901,9.59358779 C6.11230534,6.71019847 8.79636641,4.62403053 11.9587786,4.62403053"
+                                                            id="Shape" fill="#EB4335"></path>
+                                                    </svg>
+                                                    Lier mon compte Google
+                                                </button>
+                                            </div>
+                                        </div>
+
+
+                                    </div>
+                                </div>
+
+
+
+
+                            </div>
+
+
+
+
+                            <div>
+                                <div class="relative">
+                                    <div class="absolute inset-0 flex items-center" aria-hidden="true">
+                                        <div class="w-full border-t border-gray-300"></div>
+                                    </div>
+                                    <div class="relative flex justify-center">
+                                        <span class="bg-white px-2 text-sm text-gray-500">Suppression du
+                                            compte</span>
+                                    </div>
+                                </div>
+                                <div class="pt-6">
+                                    <div class="flex space-x-1 items-center justify-between">
+                                        <div class="flex items-center gap-2">
                                             <input type="radio"
                                                 class="form-radio text-red-600 border-red-400 focus:border-red-500 focus:ring-red-200 h-5 w-5"
                                                 name="choice">
                                             <label for="push-everything" class="block text-sm font-medium leading-6">Je
                                                 confirme la supression
-                                                de mon compte (action irréversible)</label>
+                                                de mon compte</label>
                                         </div>
-                                        <div class="flex justify-end pt-4">
-                                            <button @click="openModal" type="submit"
-                                                class="rounded-md bg-red-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500">Supprimer</button>
-                                        </div>
+                                        <button @click="openModal" type="submit"
+                                            class="rounded-md bg-red-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500">Supprimer</button>
                                     </div>
                                 </div>
                             </div>
+
                         </div>
                     </div>
                     <div v-if="activeSection === 'subscription'"
@@ -446,6 +561,8 @@ let timerId = ref(null);
 let activeSection = ref('preferences'); // Default active section
 let bgColor = ref(localStorage.getItem('bgColor') || '');
 let userData = ref('');
+let emailInput = ref('');
+let emailLinked = ref([]);
 let newPassword = ref('');
 let confirmPassword = ref('');
 
@@ -467,9 +584,29 @@ const billingInfo = ref({
 
 onMounted(() => {
     document.addEventListener("keydown", handleKeyDown);
+    fetchEmailLinked();
     fetchUserData();
     getBackgroundColor();
 })
+
+async function fetchEmailLinked() {
+    // const requestOptions = {
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // };
+
+    // try {
+    //     const response = await fetchWithToken(`${API_BASE_URL}user/preferences/emails_linked/`, requestOptions);
+    //     emailLinked.value = response;
+    // } catch (error) {
+    //     // Show the pop-up
+    //     backgroundColor = 'bg-red-300';
+    //     notificationTitle = 'Erreur récupération des emails liés';
+    //     notificationMessage = error.message;
+    //     displayPopup();
+    // }
+}
 
 async function submitBillingInfo() {
     const emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -528,7 +665,7 @@ async function submitBillingInfo() {
             // Show the pop-up
             backgroundColor = 'bg-red-300';
             notificationTitle = 'Erreur lors de la mise à jour des informations de facturation';
-            notificationMessage = error;
+            notificationMessage = error.message;
             displayPopup();
         }
         closeBillingModal();

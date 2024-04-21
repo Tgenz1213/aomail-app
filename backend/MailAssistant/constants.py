c488e6ac9c240da37fc24871defb2091196ca2e4
teloryfrozy <raptor.frozy@gmail.com>
2024-04-21 08:12:11 +0000

    Add llama creds and llama py file

--- backend/MailAssistant/constants.py
+++ backend/MailAssistant/constants.py
@@ -40,6 +40,7 @@ STRIPE_PRICES = {
 OPENAI_CREDS = json.load(open(f"{CREDS_PATH}openai_creds.json"))
 MISTRAL_CREDS = json.load(open(f"{CREDS_PATH}mistral_creds.json"))
 CLAUDE_CREDS = json.load(open(f"{CREDS_PATH}claude_creds.json"))
+LLAMA_CREDS = json.load(open(f"{CREDS_PATH}llama_creds.json"))
 HUMAN = "\n\nHuman: "
 ASSISTANT = "Assistant:"
 IMPORTANT = "Important"

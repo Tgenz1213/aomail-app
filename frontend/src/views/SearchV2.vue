99efd1579284d4b4775ca84cab4f7e5c6c828517
teloryfrozy <raptor.frozy@gmail.com>
2024-05-07 08:11:29 +0000

    Implement 2 inputs for search: FE

--- frontend/src/views/SearchV2.vue
+++ frontend/src/views/SearchV2.vue
@@ -85,46 +85,49 @@
                         (optionnel)</label>
                     </div>
                     <div class="relative items-stretch mt-2">
-                     
-                      
+
+
                       <Combobox as="div" v-model="selectedPerson">
-      <ComboboxInput
-        class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-12 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-gray-500 sm:text-sm sm:leading-6"
-        @input="query = $event.target.value" :display-value="(person) => person?.username" />
-      <ComboboxButton class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
-        <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-      </ComboboxButton>
+                        <ComboboxInput
+                          class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-12 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-gray-500 sm:text-sm sm:leading-6"
+                          @input="query = $event.target.value" :display-value="(person) => person?.username" />
+                        <ComboboxButton
+                          class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+                          <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                        </ComboboxButton>
 
-      <ComboboxOptions v-if="filteredPeople.length > 0"
-        class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-        <ComboboxOption v-for="person in filteredPeople" :value="person" :key="person" as="template" v-slot="{ active, selected }">
-          <li @click="toggleSelection(person)"
-            :class="['relative cursor-default select-none py-2 pl-3 pr-9', active ? 'bg-gray-500 text-white' : 'text-gray-900']">
-            <div class="flex">
-              <span :class="['truncate', selected && 'font-semibold']">
-                {{ person.username }}
-              </span>
-              <span :class="['ml-2 truncate text-gray-500', active ? 'text-indigo-200' : 'text-gray-500']">
-                &lt;{{ person.email }}&gt;
-              </span>
-            </div>
-            <span v-if="selected"
-              :class="['absolute inset-y-0 right-0 flex items-center pr-4', active ? 'text-white' : 'text-gray-500']">
-              <CheckIcon class="h-5 w-5" aria-hidden="true" />
-            </span>
-          </li>
-        </ComboboxOption>
-      </ComboboxOptions>
-    </Combobox>
+                        <ComboboxOptions v-if="filteredPeople.length > 0"
+                          class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                          <ComboboxOption v-for="person in filteredPeople" :value="person" :key="person" as="template"
+                            v-slot="{ active, selected }">
+                            <li @click="toggleSelection(person)"
+                              :class="['relative cursor-default select-none py-2 pl-3 pr-9', active ? 'bg-gray-500 text-white' : 'text-gray-900']">
+                              <div class="flex">
+                                <span :class="['truncate', selected && 'font-semibold']">
+                                  {{ person.username }}
+                                </span>
+                                <span
+                                  :class="['ml-2 truncate text-gray-500', active ? 'text-indigo-200' : 'text-gray-500']">
+                                  &lt;{{ person.email }}&gt;
+                                </span>
+                              </div>
+                              <span v-if="selected"
+                                :class="['absolute inset-y-0 right-0 flex items-center pr-4', active ? 'text-white' : 'text-gray-500']">
+                                <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                              </span>
+                            </li>
+                          </ComboboxOption>
+                        </ComboboxOptions>
+                      </Combobox>
 
-    <div v-if="selectedRecipients.length > 0" class="mt-2 flex flex-wrap">
-      <span v-for="recipient in selectedRecipients" :key="recipient.email"
-        class="bg-gray-200 px-2 py-1.5 rounded-full text-sm font-semibold mr-2 mb-2">
-        {{ recipient.username }}
-        <button @click="removeRecipient(recipient)"
-          class="ml-1 text-red-600 focus:outline-none hover:text-red-800">&times;</button>
-      </span>
-    </div>
+                      <div v-if="selectedRecipients.length > 0" class="mt-2 flex flex-wrap">
+                        <span v-for="recipient in selectedRecipients" :key="recipient.email"
+                          class="bg-gray-200 px-2 py-1.5 rounded-full text-sm font-semibold mr-2 mb-2">
+                          {{ recipient.username }}
+                          <button @click="removeRecipient(recipient)"
+                            class="ml-1 text-red-600 focus:outline-none hover:text-red-800">&times;</button>
+                        </span>
+                      </div>
 
 
 
@@ -139,49 +142,37 @@
                         (optionnel)</label>
                     </div>
                     <div class="relative items-stretch mt-2">
-                      
-<!-- TODO: a LIST of attachments choices -->
+
+                      <!-- TODO: a LIST of attachments choices -->
                       <Listbox as="div" v-model="attachmentSelected">
-    <ListboxButton
-      class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 sm:text-sm sm:leading-6"
-    >
-      <span class="block truncate">{{ attachmentSelected ? attachmentSelected.name : 'Aucune' }}</span>
-      <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-        <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-      </span>
-    </ListboxButton>
-    <transition
-      leave-active-class="transition ease-in duration-100"
-      leave-from-class="opacity-100"
-      leave-to-class="opacity-0"
-    >
-      <ListboxOptions
-        class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
-      >
-        <ListboxOption
-          as="template"
-          v-for="type in attachmentTypes"
-          :key="type.extension"
-          :value="type"
-          v-slot="{ active, attachmentSelected }"
-        >
-          <li
-            :class="[active ? 'bg-gray-500 text-white' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-3 pr-9']"
-          >
-            <span :class="[attachmentSelected ? 'font-semibold' : 'font-normal', 'block truncate']">
-              {{ type.name }} {{ type.extension }}
-            </span>
-            <span
-              v-if="attachmentSelected"
-              :class="[active ? 'text-white' : 'text-gray-500', 'absolute inset-y-0 right-0 flex items-center pr-4']"
-            >
-              <CheckIcon class="h-5 w-5" aria-hidden="true" />
-            </span>
-          </li>
-        </ListboxOption>
-      </ListboxOptions>
-    </transition>
-  </Listbox>
+                        <ListboxButton
+                          class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 sm:text-sm sm:leading-6">
+                          <span class="block truncate">{{ attachmentSelected ? attachmentSelected.name : 'Aucune'
+                            }}</span>
+                          <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                            <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                          </span>
+                        </ListboxButton>
+                        <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100"
+                          leave-to-class="opacity-0">
+                          <ListboxOptions
+                            class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                            <ListboxOption as="template" v-for="type in attachmentTypes" :key="type.extension"
+                              :value="type" v-slot="{ active, attachmentSelected }">
+                              <li
+                                :class="[active ? 'bg-gray-500 text-white' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-3 pr-9']">
+                                <span :class="[attachmentSelected ? 'font-semibold' : 'font-normal', 'block truncate']">
+                                  {{ type.name }} {{ type.extension }}
+                                </span>
+                                <span v-if="attachmentSelected"
+                                  :class="[active ? 'text-white' : 'text-gray-500', 'absolute inset-y-0 right-0 flex items-center pr-4']">
+                                  <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                                </span>
+                              </li>
+                            </ListboxOption>
+                          </ListboxOptions>
+                        </transition>
+                      </Listbox>
 
 
 
@@ -275,7 +266,7 @@
 </template>
 
 <script setup>
-import { ref, provide, computed, inject, watch, nextTick, onMounted } from 'vue';
+import { ref, computed, nextTick, onMounted } from 'vue';
 import Navbar from '../components/AppNavbar7.vue';
 import Navbar2 from '../components/AppNavbar8.vue';
 import { fetchWithToken, getBackgroundColor } from '../router/index.js';
@@ -343,7 +334,7 @@ const selectedPerson = ref(null)
 const selectedRecipients = ref([])
 
 const filteredPeople = computed(() =>
-queryGetContacts.value === ''
+  queryGetContacts.value === ''
     ? contacts
     : contacts.filter((person) => {
       return person.username.toLowerCase().includes(queryGetContacts.value.toLowerCase())
@@ -425,17 +416,23 @@ async function handleAIClick() {
 }
 
 async function searchEmails() {
-
-
-  console.log(selectedRecipients.value);
-  console.log(attachmentSelected.value);
-
-  loading();
-  scrollToBottom();
+  // TODO: add a list choice and not a UNIQUE choice
+  let fileExt = "";
+  for (const key in attachmentSelected.value) {
+    if (key === "extension") {
+      fileExt = attachmentSelected.value[key];
+    }
+  }
 
   // TODO
   // une case par compte à cocher (email)
   // un bouton cocher TOUS (email)
+  let toAddresses = selectedRecipients.value.map(recipient => recipient.email);
+
+
+  loading();
+  scrollToBottom();
+
   const requestOptions = {
     method: 'POST',
     headers: {
@@ -446,9 +443,9 @@ async function searchEmails() {
       from_addresses: [],
       to_addresses: [],
       subject: "",
-      body:"",
+      body: "",
       date_from: "",
-      file_extensions: ["pdf", "png"],
+      file_extensions: [fileExt],
       advanced: false,
       search_in: {},
       query: query.value,

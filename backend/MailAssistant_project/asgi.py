d50e2379afab4d760f8528737b277ce78ea4b9b5
teloryfrozy <raptor.frozy@gmail.com>
2024-05-19 04:39:01 +0000

    Revert "Merge start implementation websocket with languages in FE"
    This reverts commit 797c7f4d70640e49e274b2330ab93efe096eddc9.

--- backend/MailAssistant_project/asgi.py
+++ backend/MailAssistant_project/asgi.py
@@ -1,35 +1,14 @@
 """
-ASGI config for Aomail project.
+ASGI (Asynchronous Server Gateway Interface) config for MailAssistant_project project.
+Django's default ASGI handler will run all your code in a synchronous thread
 
-This module configures ASGI for the Aomail application to handle both
-synchronous HTTP and asynchronous WebSocket connections.
-
-Documentation:
+For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
-
 import os
-import django
 from django.core.asgi import get_asgi_application
-from django.urls import re_path
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from backend.MailAssistant_project.websocket import ClientWebsocket
 
-# Setting the default Django settings module for the 'asgi' application.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MailAssistant_project.settings")
-django.setup()
 
-# Define ASGI application protocol type routes handling HTTP and WebSocket requests.
-application = ProtocolTypeRouter(
-    {
-        "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(
-                [
-                    re_path(r"^ws/aomail/$", ClientWebsocket.as_asgi()),
-                ]
-            )
-        ),
-    }
-)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MailAssistant_project.settings')
+
+application = get_asgi_application()

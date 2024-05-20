72c0af6ab48bae4d8e0cf50441c7e017b197fdbe
teloryfrozy <raptor.frozy@gmail.com>
2024-05-20 09:19:39 +0000

    🪄  add: GoogleListener model: BE

--- backend/MailAssistant/schedule_tasks.py
+++ backend/MailAssistant/schedule_tasks.py
@@ -4,9 +4,11 @@ File handling schedule tasks ran in django settings
 NOT WORKING NOR TESTED
 """
 
-from django.core.management.base import BaseCommand
 from django.utils import timezone
 import logging
+
+from backend.MailAssistant.models import SocialAPI
+from backend.MailAssistant.email_providers import google_api
 
 
 ######################## LOGGING CONFIGURATION ########################
@@ -14,17 +16,15 @@ LOGGER = logging.getLogger(__name__)
 
 
 ######################## SCHEDULED TASKS ########################
-class Command(BaseCommand):
+def update_subscription_status(self):
+    """Update subscription status"""
+    from .models import Subscription
 
-    def update_subscription_status(self):
-        """Update subscription status"""
-        from .models import Subscription
+    today = timezone.now().date()
+    subscriptions = Subscription.objects.filter(end_date__lt=today, is_active=True)
 
-        today = timezone.now().date()
-        subscriptions = Subscription.objects.filter(end_date__lt=today, is_active=True)
-
-        LOGGER.info("Starting update_subscription_status script")
-        for subscription in subscriptions:
-            subscription.is_active = False
-            subscription.save()
-            LOGGER.info(f"Subscription {subscription.id} status updated to inactive.")
+    LOGGER.info("Starting update_subscription_status script")
+    for subscription in subscriptions:
+        subscription.is_active = False
+        subscription.save()
+        LOGGER.info(f"Subscription {subscription.id} status updated to inactive.")

dd18cb3b4eb98a24e27a927c49d040a39dff2eb9
Theo <theo@vps-feaf3d65.vps.ovh.net>
2024-05-27 19:55:22 +0000

    🎨 Settings preference UI updated

--- frontend/src/views/SettingsV1.vue
+++ frontend/src/views/SettingsV1.vue
@@ -564,12 +564,11 @@
                     <div v-if="activeSection === 'preferences'"
                         class="flex-1 section">
                         <!-- TO DO : CENTER -->
-                        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-10">
-
-                            <div class="mx-auto max-w-lg">
-                                <!-- Content goes here -->
-                                <div class="flex">
-                                    <div class="flex-col">
+                        <div class="mx-auto max-w-7xl h-full px-4 sm:px-6 lg:px-8 pt-10">
+                            <!-- Content goes here -->
+                            <div class="flex flex-col h-full pb-6">
+                                <div class="flex gap-x-10 w-full">
+                                    <div class="flex-1 flex flex-col">
                                         <div class="relative">
                                             <div class="absolute inset-0 flex items-center" aria-hidden="true">
                                                 <div class="w-full border-t border-gray-300"></div>
@@ -578,13 +577,11 @@
                                                 <span class="bg-white px-2 text-sm text-gray-500">LANGUAGES</span>
                                             </div>
                                         </div>
-                                        <div class="pt-6 pb-10">
-                                            <div class="pt-6">
-                                                <LanguageChange></LanguageChange>
-                                            </div>
+                                        <div class="pt-10 pb-10">
+                                            <LanguageChange></LanguageChange>
                                         </div>
                                     </div>
-                                    <div class="flex-col">
+                                    <div class="flex-1 flex flex-col">
                                         <div class="relative">
                                             <div class="absolute inset-0 flex items-center" aria-hidden="true">
                                                 <div class="w-full border-t border-gray-300"></div>
@@ -594,25 +591,24 @@
                                                     $t('settings_navigation.Theme') }}</span>
                                             </div>
                                         </div>
-                                        <div class="pt-6 pb-10">
-                                            <div class="relative items-stretch mt-2">
+                                        <div class="pt-10 pb-10">
+                                            <div class="relative items-stretch">
                                                 <theme></theme>
                                             </div>
                                         </div>
                                     </div>
-                                    <!--
-                                    <div class="relative">
-                                        <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                                            <div class="w-full border-t border-gray-300"></div>
-                                        </div>
-                                        <div class="relative flex justify-center">
-                                            <span class="bg-white px-2 text-sm text-gray-500">{{
-                                                $t('settings_navigation2.Colors') }}</span>
-                                        </div>
+                                </div>
+                                <div
+                                    class="flex-1 w-full h-full rounded-lg border-2 border-dashed border-gray-300 hover:border-gray-400 text-center">
+                                    <div class="flex flex-col h-full items-center justify-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1" stroke="currentColor" class="w-12 h-12 mx-auto text-gray-400">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z" />
+                                        </svg>
+                                        <span class="mt-2 block text-sm font-semibold text-gray-900">En cours de
+                                            construction</span>
                                     </div>
-                                    <div class="pt-6">
-                                        <color :initialColor="bgColor" @colorSelected="handleColorChange"></color>
-                                    </div>-->
                                 </div>
                             </div>
                         </div>

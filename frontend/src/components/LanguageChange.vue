14a606973b97209f565f58692709e94eb23ce4ed
teloryfrozy <raptor.frozy@gmail.com>
2024-05-27 10:25:00 +0000

    🪲  fix: language dropdown list width: FE

--- frontend/src/components/LanguageChange.vue
+++ frontend/src/components/LanguageChange.vue
@@ -2,30 +2,36 @@
   <ShowNotification :showNotification="showNotification" :notificationTitle="notificationTitle"
     :notificationMessage="notificationMessage" :backgroundColor="backgroundColor" @dismiss-popup="dismissPopup" />
 
-  <Listbox as="div" v-model="selectedLanguage">
-    <ListboxButton
-      class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-800 sm:text-sm sm:leading-6">
-      <span class="block truncate">{{ $t(selectedLanguage.i18nKey) }}</span>
-      <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-        <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-      </span>
-    </ListboxButton>
+  <div class="relative">
+    <Listbox as="div" v-model="selectedLanguage">
+      <ListboxButton
+        class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-800 sm:text-sm sm:leading-6">
+        <span class="block truncate">{{ $t(selectedLanguage.i18nKey) }}</span>
+        <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+          <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+        </span>
+      </ListboxButton>
 
-    <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100"
-      leave-to-class="opacity-0">
-      <ListboxOptions
-        class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-        <ListboxOption as="template" v-for="language in languages" :key="language.key" :value="language">
-          <li :class="getOptionClasses(active, selected)">
-            <span :class="getLabelClasses(selected)">{{ $t(language.i18nKey) }}</span>
-            <span v-if="selected" :class="getCheckIconClasses(active)">
-              <CheckIcon class="h-5 w-5" aria-hidden="true" />
-            </span>
-          </li>
-        </ListboxOption>
-      </ListboxOptions>
-    </transition>
-  </Listbox>
+      <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100"
+        leave-to-class="opacity-0">
+        <ListboxOptions
+          class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+          <ListboxOption as="template" v-for="language in languages" :key="language.key" :value="language"
+            v-slot="{ active, selected }">
+            <li
+              :class="[active ? 'bg-gray-800 text-white' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-3 pr-9']">
+              <span :class="[selected ? 'font-semibold' : 'font-normal', 'block truncate']">{{ $t(language.i18nKey)
+                }}</span>
+              <span v-if="selected"
+                :class="[active ? 'text-white' : 'text-gray-500', 'absolute inset-y-0 right-0 flex items-center pr-4']">
+                <CheckIcon class="h-5 w-5" aria-hidden="true" />
+              </span>
+            </li>
+          </ListboxOption>
+        </ListboxOptions>
+      </transition>
+    </Listbox>
+  </div>
 </template>
 
 <script setup>

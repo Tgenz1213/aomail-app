ebc39a3c5569999b9df7094752edc850aafec8fc
teloryfrozy <raptor.frozy@gmail.com>
2024-05-27 10:04:00 +0000

    🪲  fix: Auto refresh language on Change - Settings: FE

--- frontend/src/components/LanguageChange.vue
+++ frontend/src/components/LanguageChange.vue
@@ -1,5 +1,7 @@
 <template>
-  <Listbox as="div" v-model="selectedLanguage" @change="updateLanguageSelection">
+  <ShowNotification :showNotification="showNotification" :notificationTitle="notificationTitle"
+    :notificationMessage="notificationMessage" :backgroundColor="backgroundColor" @dismiss-popup="dismissPopup" />
+  <Listbox as="div" v-model="selectedLanguage">
     <ListboxButton
       class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-800 sm:text-sm sm:leading-6">
       <span class="block truncate">{{ $t(selectedLanguage.i18nKey) }}</span>
@@ -28,7 +30,11 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+import ShowNotification from '../components/ShowNotification.vue';
+import { fetchWithToken } from '../router/index.js';
+import { API_BASE_URL } from '@/main';
+import i18n from '@/main';
 import { Listbox, ListboxButton, ListboxOptions, ListboxOption, ChevronUpDownIcon, CheckIcon } from '@headlessui/vue';
 
 const languages = ref([
@@ -44,10 +50,77 @@ const languages = ref([
 const storedLanguageKey = localStorage.getItem("language");
 const languageIndex = languages.value.findIndex(lang => lang.key === storedLanguageKey);
 const selectedLanguage = ref(languages.value[languageIndex] || languages.value[1]);
-const emit = defineEmits(['languageUpdated']);
 
-const updateLanguageSelection = (newLanguage) => {
+
+
+// Variables to display a notification
+let showNotification = ref(false);
+let notificationTitle = ref('');
+let notificationMessage = ref('');
+let backgroundColor = ref('');
+let timerId = ref(null);
+
+
+function dismissPopup() {
+  showNotification.value = false;
+  // Cancel the timer
+  clearTimeout(timerId.value);
+}
+function displayPopup() {
+  showNotification.value = true;
+
+  timerId.value = setTimeout(() => {
+    dismissPopup();
+  }, 4000);
+}
+
+async function updateLanguageSelection(newLanguage) {
   selectedLanguage.value = newLanguage;
-  emit('languageUpdated', newLanguage);
-};
+  const newLanguageKey = newLanguage.key;
+  const currentLanguage = localStorage.getItem('language');
+
+  if (newLanguageKey === currentLanguage) {
+    return;
+  }
+  localStorage.setItem('language', newLanguageKey);
+  const storedLanguage = languages.value.find(lang => lang.key === newLanguageKey);
+  selectedLanguage.value = storedLanguage;
+
+  const requestOptions = {
+    headers: { 'Content-Type': 'application/json' },
+    method: "POST",
+    body: JSON.stringify({ language: newLanguageKey })
+  };
+
+  try {
+    const response = await fetchWithToken(`${API_BASE_URL}user/set_language/`, requestOptions);
+
+    if ("error" in response) {
+      // Show the pop-up
+      backgroundColor = 'bg-red-300';
+      notificationTitle.value = 'Error get language';
+      notificationMessage.value = response.error;
+      displayPopup();
+    } else if (response.message == "Language updated successfully") {
+      // Update the i18n locale
+      i18n.global.locale = newLanguageKey;
+
+      // Show the pop-up
+      backgroundColor = 'bg-green-300';
+      notificationTitle.value = 'Succès!';
+      notificationMessage.value = 'Language updated successfully';
+      displayPopup();
+    }
+  } catch (error) {
+    // Show the pop-up
+    backgroundColor = 'bg-red-300';
+    notificationTitle.value = 'Error get language';
+    notificationMessage.value = error.message;
+    displayPopup();
+  }
+}
+
+watch(selectedLanguage, (newLanguage) => {
+  updateLanguageSelection(newLanguage);
+});
 </script>

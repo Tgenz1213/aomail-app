a5a221cd8adf7a6ccac859b2afc42209b8e76908
Theo <theo@vps-feaf3d65.vps.ovh.net>
2024-05-28 19:58:10 +0000

    🎨 Design Home right menu fixed

--- frontend/src/views/HomeV15.vue
+++ frontend/src/views/HomeV15.vue
@@ -52,27 +52,7 @@
                 </div>
             </div>
         </transition>
-        <!--
-    <div class="pb-1 lg:pl-20 bg-gray-100">
-        <div class="grid grid-cols-8 gap-6 h-72 items-center divide-x-8 divide-indigo-900 bg-blue-400">
-            <div class="col-span-3 h-full bg-red-500">
-                    
-                    <div class="flex">
-                        <div class="flex-shrink-0 self-center">
-                            <span class="inline-flex h-14 w-14 items-center justify-center rounded-full bg-indigo-800">
-                                <span class="text-lg font-medium leading-none text-white">AO</span>
-                            </span>
-                        </div>
-                        <div>
-                            <p class="mt-1" id="animated-text" ref="animatedText"></p>
-                        </div>
-                    </div>
-                </div>
-            <div class="col-span-5 h-full bg-red-500">
-                <p>Test</p>
-            </div>
-        </div>
-    </div>-->
+
         <div class="flex flex-col justify-center items-center h-screen"><!-- DO NOT DELETE : 'bg-gray-900'-->
             <div class="flex h-full w-full">
                 <div class="w-[90px] bg-white ring-1 shadow-sm ring-black ring-opacity-5">
@@ -1395,51 +1375,54 @@
                         </div>
                     </div>
                 </div>
-
-                <!--
-                                <span class="inline-flex h-14 w-14 items-center justify-center rounded-full bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-rose-400 via-amber-400 to-rose-200">
-                                    <span class="text-lg font-medium leading-none text-white">AO</span>
-                                </span>-->
-                <!--
-                                    <span
-                                        class="inline-flex h-14 w-14 items-center justify-center rounded-full overflow-hidden">
-                                        <img :src="happy_icon" alt="New Emails Icon"
-                                            class="max-w-full max-h-full rounded-full">
-                                    </span>-->
-
-
-<div class="bg-gray-50 p-4 ring-1 shadow-sm ring-black ring-opacity-5 h-full flex flex-col">
-    <button @click="toggleVisibility"
-                        class="bg-gray-700 text-white px-4 py-2 rounded mb-4 flex items-center inline-flex pr-10">
-                        <svg v-if="isHidden" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5" />
-                        </svg>
-                        <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
-                        </svg>
-                    </button>
-    <div v-if="!isHidden" class="w-full sm:w-[350px] md:w-[400px] lg:w-[450px] flex-grow">
-        <div class="flex flex-col h-full">
-            <div class="flex-grow">
-                <!-- Upper content placeholder for spacing -->
-            </div>
-            <div class="flex flex-col justify-end px-4 py-6">
-                <textarea id="dynamicTextarea" @keydown.enter="handleEnterKey"
-                          @input="adjustHeight" v-model="textareaValue"
-                          class="overflow-y-hidden pt-3 pl-5 w-full border-transparent bg-transparent text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6 focus:border-transparent focus:bg-transparent focus:ring-0"
-                          placeholder="Instruction">
-                </textarea>
-                <div class="flex justify-end mt-3">
-                    <button type="button" class="w-[80px] rounded bg-gray-700 px-2.5 py-1.5 text-sm text-white shadow-sm hover:bg-gray-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">Envoyer</button>
+                <div class="bg-gray-50 ring-1 shadow-sm ring-black ring-opacity-5 h-full flex flex-col">
+                    <div class="p-4 flex w-full">
+                        <button @click="toggleVisibility"
+                            class="bg-gray-200 text-gray-500 px-4 py-2 rounded w-full items-center inline-flex pr-10">
+                            <svg v-if="isHidden" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5" />
+                            </svg>
+                            <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div v-if="!isHidden" class="w-[325px] 2xl:w-[450px] flex-grow">
+                        <div class="flex flex-col h-full">
+                            <div class="flex-grow">
+                                <div class="flex p-5">
+                                    <div class="mr-4 flex-shrink-0 self-center">
+                                        <span
+                                            class="inline-flex h-14 w-14 items-center justify-center rounded-full bg-gray-900 text-white">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+                                            </svg>
+                                        </span>
+                                    </div>
+                                    <div>
+                                        <p class="mt-1" id="animated-text" ref="animatedText"></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex flex-col justify-end h-[160px] border-t">
+                                <textarea id="dynamicTextarea" @keydown.enter="handleEnterKey"
+                                        @input="adjustHeight" v-model="textareaValue"
+                                        class="overflow-y-hidden flex flex-1 pt-3 pl-5 w-full border-transparent bg-transparent text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6 focus:border-transparent focus:bg-transparent focus:ring-0"
+                                        placeholder="Instruction">
+                                </textarea>
+                                <div class="flex justify-end m-3">
+                                    <button type="button" class="w-[80px] rounded bg-gray-700 px-2.5 py-1.5 text-sm text-white shadow-sm hover:bg-gray-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">Envoyer</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
             </div>
         </div>
         <!-- Category Modal -->

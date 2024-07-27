db43777e416b0b9397fa956f72ae748cc26e347e
Theo <theo@vps-feaf3d65.vps.ovh.net>
2024-07-27 17:25:21 +0000

    🎨 UI : Search : Added hover design summary UX indicator

--- frontend/src/views/SearchV2.vue
+++ frontend/src/views/SearchV2.vue
@@ -633,21 +633,44 @@
                   <template v-if="sortedEmailList.length > 0">
                     <ul class="space-y-4 pr-4">
                       <template v-for="(email, index) in sortedEmailList" :key="email.id">
-                        <li class="flex justify-between items-center py-4 email-item">
+                        <li class="group flex justify-between items-center py-2 email-item">
                           <div class="flex flex-col justify-center">
                             <span class="font-bold text-sm">
                               {{ email.sender.name }}
                               ({{ email.sender.email }})
                             </span>
                             <span class="text-sm">{{ email.subject }} - {{ email.oneLineSummary }}</span>
-                            <div class="mt-1 space-x-2">
+                            <div class="mt-1 flex space-x-2">
                               <!-- Priority flag -->
-                              <span v-if="email.priority === 'important'" class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">Important</span>
+                              <span v-if="email.priority === 'important'" class="inline-flex items-center rounded-md bg-orange-50 px-2 py-1 text-xs font-medium text-orange-700 ring-1 ring-inset ring-orange-600/10">Important</span>
                               <span v-else-if="email.priority === 'informative'" class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">Informative</span>
                               <span v-else class="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">Useless</span>
                               
                               <!-- Category flag (always gray) -->
                               <span class="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">{{ email.category }}</span>
+                              
+                              <!-- Optional hover UX info -->
+                              <span
+                                v-bind:class="{
+                                  'hidden group-hover:block px-1.5 text-white shadow rounded-xl inline-flex': true,
+                                  'bg-orange-300': email.priority === 'important',
+                                  'bg-blue-300': email.priority === 'informative',
+                                  'bg-gray-300': email.priority !== 'important' && email.priority !== 'informative'
+                                }"
+                              >
+                                <div class="flex gap-x-1 items-center justify-center h-full">
+                                  <svg xmlns="http://www.w3.org/2000/svg"
+                                      fill="none" viewBox="0 0 24 24"
+                                      stroke-width="1.5"
+                                      stroke="currentColor"
+                                      class="w-4 h-4">
+                                    <path stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672Zm-7.518-.267A8.25 8.25 0 1 1 20.25 10.5M8.288 14.212A5.25 5.25 0 1 1 17.25 10.5" />
+                                  </svg>
+                                  <p class="text-xs">{{ $t('constants.userActions.clickToSeeTheSummary') }}</p>
+                                </div>
+                              </span>
                             </div>
                           </div>
 

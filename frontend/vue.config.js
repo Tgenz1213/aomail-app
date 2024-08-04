28576cbe98f677d2ce11049a6e1078718317d5e5
teloryfrozy <raptor.frozy@gmail.com>
2024-08-04 22:38:39 +0000

    🐋 docker: fix vue config + DockerFile

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
     transpileDependencies: true,
+    devServer: {
+        server: {
+            type: "https",
+        },
+        headers: {
+            "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+            "Cross-Origin-Embedder-Policy": "unsafe-none",
+        },
+        liveReload: true,
+        watchFiles: {
+            options: {
+                usePolling: true,
+            },
+        },
+        allowedHosts: ["aomail.ai", "jean.aomail.ai", "augustin.aomail.ai", "theo.aomail.ai", "localhost"],
+    },
 });

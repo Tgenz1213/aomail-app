1969959fab7605dbe61301de80edacc5fee94d95
Theo <theo@vps-feaf3d65.vps.ovh.net>
2024-08-06 22:18:31 +0000

    🐋 Merge => Fixed start build with docker

--- backend/aomail/apps.py
+++ backend/aomail/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
-class MailassistantConfig(AppConfig):
+class AomailConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'MailAssistant'
+    name = 'aomail'

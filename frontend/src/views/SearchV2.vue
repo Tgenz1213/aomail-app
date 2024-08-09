63da1b5f5f8da0fcb1f43799c7ce0e91fc800adb
Theo <theo@vps-feaf3d65.vps.ovh.net>
2024-08-09 15:48:06 +0000

    🎨 Search fixed traduction error first info in chat

--- frontend/src/views/SearchV2.vue
+++ frontend/src/views/SearchV2.vue
@@ -1433,7 +1433,7 @@ async function animateText(text, target) {
 
 async function askQueryUser() {
   // const message = "Bonjour, quel email recherchez vous. Pouvez vous me donner des informations sur l'email ?"; THIS IS FOR AI SEARCH NOT TREE KNOWLEDGE
-  const message = "Bonjour, quelle information souhaitez vous chercher ?"; // THIS IS FOR TREE KNOWLEDGE
+  const message = t('searchPage.searchInfoPrompt'); // THIS IS FOR TREE KNOWLEDGE
   const ai_icon = '<path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />';
   await displayMessage(message, ai_icon);
 

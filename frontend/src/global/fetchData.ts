c4cad044b586da3a14b7ce8b140aedafbe1e2ec3
Teh45 <theohubert3@gmx.com>
2024-08-12 09:07:49 +0000

    🔀 Added in global putData and deleteData

--- frontend/src/global/fetchData.ts
+++ frontend/src/global/fetchData.ts
@@ -84,3 +84,82 @@ export async function postData(path: string, body: Record<string, any>): Promise
         };
     }
 }
+
+export async function deleteData(path: string): Promise<FetchDataResult> {
+    const requestOptions = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+
+    try {
+        const response = await fetchWithToken(`${API_BASE_URL}${path}`, requestOptions);
+
+        if (!response) {
+            return {
+                success: false,
+                error: "No server response",
+            };
+        }
+
+        const data = await response.json();
+
+        if (200 <= response.status && response.status <= 299) {
+            return {
+                success: true,
+                data: data,
+            };
+        } else {
+            return {
+                success: false,
+                error: data.error ? data.error : "",
+            };
+        }
+    } catch (error) {
+        return {
+            success: false,
+            error: error instanceof Error ? error.message : String(error),
+        };
+    }
+}
+
+export async function putData(path: string, body: Record<string, any>): Promise<FetchDataResult> {
+    const requestOptions = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    };
+
+    try {
+        const response = await fetchWithToken(`${API_BASE_URL}${path}`, requestOptions);
+
+        if (!response) {
+            return {
+                success: false,
+                error: "No server response",
+            };
+        }
+
+        const data = await response.json();
+
+        if (200 <= response.status && response.status <= 299) {
+            return {
+                success: true,
+                data: data,
+            };
+        } else {
+            return {
+                success: false,
+                error: data.error ? data.error : "",
+            };
+        }
+    } catch (error) {
+        return {
+            success: false,
+            error: error instanceof Error ? error.message : String(error),
+        };
+    }
+}

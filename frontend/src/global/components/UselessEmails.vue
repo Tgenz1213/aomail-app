c374453a172ee44828dd4c379f65150e00bf6944
Teh45 <theohubert3@gmx.com>
2024-08-13 22:21:39 +0000

    🔀 Moved some home components to global components

--- frontend/src/global/components/UselessEmails.vue
+++ frontend/src/global/components/UselessEmails.vue
@@ -1,12 +1,12 @@
 <template>
-    <div v-if="emails.length > 0" class="px-6 py-6 ">
-      <div class="bg-orange-100 bg-opacity-90 rounded-md">
-        <div class="flex px-3 py-2">
-          <p class="flex-1 text-sm font-semibold leading-6 text-orange-600">
-            {{ $t('constants.ruleModalConstants.important') }}
+    <div v-if="emails.length > 0" class="px-6 pb-6">
+      <div class="bg-gray-100 bg-opacity-90 rounded-md">
+        <div class="flex px-2 py-2">
+          <p class="flex-1 text-sm font-semibold leading-6 text-gray-600">
+            {{ $t('constants.ruleModalConstants.useless') }}
           </p>
           <div class="ml-auto">
-            <exclamation-triangle-icon class="w-6 h-6 text-orange-500" />
+            <trash-icon class="w-6 h-6 text-gray-500" />
           </div>
         </div>
       </div>
@@ -23,16 +23,16 @@
         </div>
         <div class="flex px-4 pt-4">
           <div class="flex">
-            <span class="inline-flex h-14 w-14 items-center justify-center rounded-full bg-orange-300">
+            <span class="inline-flex h-14 w-14 items-center justify-center rounded-full bg-gray-400">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z" />
               </svg>
             </span>
           </div>
           <div class="ml-6 flex-grow">
-            <div class="overflow-hidden border-l-4 border-orange-300 hover:rounded-l-xl" style="overflow: visible;">
+            <div class="overflow-hidden border-l-4 hover:rounded-l-xl border-gray-300" style="overflow: visible;">
               <ul role="list" class="divide-y divide-gray-200">
-                <li v-for="email in emailsByDate" :key="email.id" class="px-6 md:py-5 2xl:py-6 hover:bg-opacity-70 grid grid-cols-10 gap-4 items-center" @mouseover="setHoveredItem(email.id)" @mouseleave="clearHoveredItem">
+                <li v-for="email in emailsByDate" :key="email.id" class="px-6 py-4 2xl:py-5 hover:bg-opacity-70 grid grid-cols-10 gap-4 items-center" @mouseover="setHoveredItem(email.id)" @mouseleave="clearHoveredItem">
                     <EmailItem 
                         :email="email" 
                         @emailUpdated="updateEmail"
@@ -48,15 +48,15 @@
   
   <script setup lang="ts">
   import { computed, ref } from 'vue';
-  import { ExclamationTriangleIcon } from '@heroicons/vue/24/outline';
+  import { TrashIcon } from '@heroicons/vue/24/outline';
   import { Email } from '@/global/types';
-  import EmailItem from './EmailItem.vue';
+  import EmailItem from '@/global/components/EmailItem.vue';
   
   const props = defineProps<{
     emails: Email[];
   }>();
-
-  const updateEmail = (updatedEmail: Email) => {
+  
+const updateEmail = (updatedEmail: Email) => {
     const index = props.emails.findIndex(e => e.id === updatedEmail.id);
     if (index !== -1) {
         props.emails[index] = updatedEmail;
@@ -67,16 +67,14 @@
   
   const groupedEmails = computed(() => {
     const grouped: Record<string, Email[]> = {};
-    props.emails.forEach(email => {
-      const sentDate = email.sentDate || 'Unknown Date';
-      if (!grouped[sentDate]) {
-        grouped[sentDate] = [];
+    /*props.emails.forEach(email => {
+      if (!grouped[email.sentDate]) {
+        grouped[email.sentDate] = [];
       }
-      grouped[sentDate].push(email);
-    });
+      grouped[email.sentDate].push(email);
+    });*/
     return grouped;
   });
-
   
   const setHoveredItem = (id: number) => {
     hoveredItemId.value = id;

16ae4fd85d4a518596d5af448b77898ecd04be48
Teh45 <theohubert3@gmx.com>
2024-08-18 13:28:55 +0000

    🔀 Home : Useless Emails Done

--- frontend/src/global/components/UselessEmails.vue
+++ frontend/src/global/components/UselessEmails.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="emails.length > 0" class="px-6 pb-6">
+    <div v-if="hasEmails" class="px-6 py-6">
       <div class="bg-gray-100 bg-opacity-90 rounded-md">
         <div class="flex px-2 py-2">
           <p class="flex-1 text-sm font-semibold leading-6 text-gray-600">
@@ -10,7 +10,46 @@
           </div>
         </div>
       </div>
-      <div v-for="(emailsByDate, date) in groupedEmails" :key="date">
+      <div class="flex gap-x-2">
+        <div class="flex gap-x-2 px-6 pt-5 w-full group" @click="toggleEmailVisibility">
+          <p
+              class="cursor-pointer"> {{ $t('homePage.youReceived') }}
+              <span
+                  class="font-semibold text-gray-900 dark:text-white hover:text-gray-700 w-full">
+                  {{ emails.length }}
+              </span>
+              <span>
+                  {{ $t('homePage.uselessEmails') }}
+              </span>
+          </p>
+          <div :class="`hidden group-hover:block px-2 py-0.5 bg-gray-300 text-white text-sm shadow rounded-xl`">
+            <div class="flex gap-x-1 items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672Zm-7.518-.267A8.25 8.25 0 1 1 20.25 10.5M8.288 14.212A5.25 5.25 0 1 1 17.25 10.5" />
+              </svg>
+              <p>{{ $t('constants.userActions.clickToSeeUselessEmails') }}</p>
+            </div>
+          </div>
+        </div>
+        <div
+            class="hidden group-hover/main:block px-2 py-0.5 bg-gray-400 text-white text-sm shadow rounded-xl">
+            <div class="flex gap-x-1 items-center">
+                <svg xmlns="http://www.w3.org/2000/svg"
+                    fill="none" viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-4 h-4">
+                    <path stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672Zm-7.518-.267A8.25 8.25 0 1 1 20.25 10.5M8.288 14.212A5.25 5.25 0 1 1 17.25 10.5" />
+                </svg>
+                <!-- remove @click="toggleEmailVisibility"-->
+                <p @click="toggleEmailVisibility"
+                    class="cursor-pointer">{{ $t('homePage.clickToSeeTheEmail') }}</p>
+            </div>
+        </div>
+      </div>
+      <div v-if="showEmailDescriptions" v-for="(emailsByDate, date) in groupedEmails" :key="date">
         <div class="pt-3 px-4">
           <div class="relative">
             <div class="absolute inset-0 z-0 flex items-center" aria-hidden="true">
@@ -32,10 +71,9 @@
           <div class="ml-6 flex-grow">
             <div class="overflow-hidden border-l-4 hover:rounded-l-xl border-gray-300" style="overflow: visible;">
               <ul role="list" class="divide-y divide-gray-200">
-                <li v-for="email in emailsByDate" :key="email.id" class="px-6 py-4 2xl:py-5 hover:bg-opacity-70 grid grid-cols-10 gap-4 items-center" @mouseover="setHoveredItem(email.id)" @mouseleave="clearHoveredItem">
+                <li v-for="email in emailsByDate" :key="email.id" class="px-6 md:py-5 2xl:py-6">
                     <EmailItem 
                         :email="email" 
-                        @emailUpdated="updateEmail"
                     />
                 </li>
               </ul>
@@ -47,7 +85,7 @@
   </template>
   
   <script setup lang="ts">
-  import { computed, ref } from 'vue';
+  import { computed, ref, watch } from 'vue';
   import { TrashIcon } from '@heroicons/vue/24/outline';
   import { Email } from '@/global/types';
   import EmailItem from '@/global/components/EmailItem.vue';
@@ -56,32 +94,38 @@
     emails: Email[];
   }>();
   
-const updateEmail = (updatedEmail: Email) => {
-    const index = props.emails.findIndex(e => e.id === updatedEmail.id);
-    if (index !== -1) {
-        props.emails[index] = updatedEmail;
-    }
-  };
-  
-  const hoveredItemId = ref<number | null>(null);
-  
+  let showEmailDescriptions = ref(false);
+  const localEmails = ref<Email[]>([]);
+
+  watch(() => props.emails, (newEmails) => {
+    localEmails.value = [...newEmails];
+  }, { immediate: true, deep: true });
+
   const groupedEmails = computed(() => {
     const grouped: Record<string, Email[]> = {};
-    /*props.emails.forEach(email => {
-      if (!grouped[email.sentDate]) {
-        grouped[email.sentDate] = [];
+    localEmails.value.forEach(email => {
+      if (!email.read) {
+        const sentDate = email.sentDate || 'Unknown Date';
+        if (!grouped[sentDate]) {
+          grouped[sentDate] = [];
+        }
+        grouped[sentDate].push(email);
       }
-      grouped[email.sentDate].push(email);
-    });*/
+    });
     return grouped;
   });
-  
-  const setHoveredItem = (id: number) => {
-    hoveredItemId.value = id;
-  };
-  
-  const clearHoveredItem = () => {
-    hoveredItemId.value = null;
-  };
+
+  const hasEmails = computed(() => {
+    return Object.keys(groupedEmails.value).length > 0;
+  });
+  function toggleEmailVisibility() {
+    showEmailDescriptions.value = !showEmailDescriptions.value;
+    /*
+    if (readEmailsInSelectedTopic() == 0) {
+        scrollToBottom();
+    } else {
+        scrollAlmostToBottom();
+    }*/
+  }
   </script>
   

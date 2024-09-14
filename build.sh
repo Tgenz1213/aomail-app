8aa20eaf8282f0779405c85e32c28c4127e38a11
teloryfrozy <raptor.frozy@gmail.com>
2024-09-14 14:33:46 +0000

    🐋 docker: add DB status checker before starting backend

--- build.sh
+++ build.sh
@@ -2,25 +2,17 @@ export BACKEND_PORT=8002
 export FRONTEND_PORT=8082
 export DB_PORT=5435
 export ENV="app"
+export NODE_ENV="production"
 export TOPIC_NAME="app_prod_sub_new_mail"
 export POSTGRES_USER="django_admin"
 export POSTGRES_PASSWORD="admin@2"
 export POSTGRES_DB="mailassistantdb"
 
-# Create the folder backend/media/pictures if it doesn't exist
-if [ ! -d "backend/media" ]; then
-    mkdir -p backend/media/pictures
-fi
-
-if [ ! -d "backend/media/labels" ]; then
-    mkdir -p backend/media/labels
-fi
- 
 # Start the containers and build if necessary
 docker compose -p ${ENV}_prod up --build -d
 
 # Wait for the backend container to be running
-container_name="${ENV}_prod-backend"
+container_name="${ENV}_prod-backend-1"
 
 # Extract the ID of the Google renew subscription
 ID=""
@@ -36,8 +28,3 @@ echo "ID found: $ID"
 # Define the cron job and add it if it doesn't exist
 CRON_JOB="0 3 * * * docker exec -i ${ENV}_prod-backend /usr/local/bin/python /app/manage.py crontab run $ID"
 (crontab -l | grep -F "$CRON_JOB") && echo "Cron job already exists" || (crontab -l; echo "$CRON_JOB") | crontab -
-
-
-# Run migrations
-docker exec -it "${container_name}" python manage.py makemigrations
-docker exec -it "${container_name}" python manage.py migrate

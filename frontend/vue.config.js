2dba0ff73fe3edbe31d791965376d8f62956952a
teloryfrozy <raptor.frozy@gmail.com>
2024-09-17 13:03:24 +0000

    🐋 docker: working frontend prod setup

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,11 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
+const TerserPlugin = require("terser-webpack-plugin");
+const WebpackObfuscator = require("webpack-obfuscator");
 
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    server: {
-      type: "https",
-    },
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
       "Cross-Origin-Embedder-Policy": "unsafe-none",
@@ -24,5 +23,40 @@ module.exports = defineConfig({
       "theo.aomail.ai",
       "localhost",
     ],
+  },
+  productionSourceMap: true,
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === "production") {
+      config.optimization = {
+        minimize: true,
+        minimizer: [
+          new TerserPlugin({
+            terserOptions: {
+              output: {
+                comments: false,
+              },
+              compress: {
+                drop_console: true,
+                drop_debugger: true,
+              },
+            },
+            extractComments: false,
+          }),
+        ],
+      };
+      config.plugins.push(
+        new WebpackObfuscator({
+          rotateStringArray: true,
+          stringArray: true,
+          stringArrayThreshold: 0.5,
+          deadCodeInjection: false,
+          deadCodeInjectionThreshold: 0.1,
+          debugProtection: false,
+          compact: true,
+          controlFlowFlattening: false,
+          controlFlowFlatteningThreshold: 0.75,
+        })
+      );
+    }
   },
 });
--- start_theo_build.sh
+++ start_theo_build.sh
@@ -2,26 +2,11 @@ export BACKEND_PORT=8001
 export FRONTEND_PORT=8081
 export DB_PORT=5433
 export ENV="theo"
+export NODE_ENV="development"
 export TOPIC_NAME="sub_new_mail"
 export POSTGRES_USER="django_admin"
 export POSTGRES_PASSWORD="admin@2"
 export POSTGRES_DB="mailassistantdb"
-
-
-
-
-# Create the folder backend/media/pictures if it doesn't exist
-if [ ! -d "backend/media" ]; then
-    mkdir -p backend/media/pictures
-fi
-
-if [ ! -d "backend/media/labels" ]; then
-    mkdir -p backend/media/labels
-fi
-
-# docker compose -p augustin_project up --build
-# use this to force install reqs or delete backend instance
-#docker compose -p augustin_project build --no-cache && docker compose -p augustin_project up
 
 # Start the containers and build if necessary
 docker compose -p ${ENV}_project up --build -d
@@ -43,8 +28,3 @@ echo "ID found: $ID"
 # Define the cron job and add it if it doesn't exist
 CRON_JOB="0 3 * * * docker exec -i ${ENV}_project-backend-1 /usr/local/bin/python /app/manage.py crontab run $ID"
 (crontab -l | grep -F "$CRON_JOB") && echo "Cron job already exists" || (crontab -l; echo "$CRON_JOB") | crontab -
-
-
-# Run migrations
-docker exec -it "${container_name}" python manage.py makemigrations
-docker exec -it "${container_name}" python manage.py migrate

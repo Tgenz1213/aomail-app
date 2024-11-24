733e6f1be811df351354a6ea3f377fb8318c9205
Teh45 <theohubert3@gmx.com>
2024-11-24 15:07:48 +0000

    🪲 Fix issue 3 : no forward attachments step 1

--- frontend/src/global/components/EmailItem.vue
+++ frontend/src/global/components/EmailItem.vue
@@ -663,6 +663,8 @@ async function transferEmail() {
 
     sessionStorage.setItem("subject", JSON.stringify(result.data.subject));
     sessionStorage.setItem("cc", result.data.cc);
+    console.log("DEBUGGGGGGGGGGGGGGGGGGGG", localEmail.value.attachments);
+    sessionStorage.setItem("attachments", JSON.stringify(localEmail.value.attachments));
     sessionStorage.setItem("decodedData", JSON.stringify(result.data.decodedData));
     sessionStorage.setItem("emailUser", JSON.stringify(result.data.emailUser));
     sessionStorage.setItem("senderEmail", JSON.stringify(localEmail.value.sender.email));

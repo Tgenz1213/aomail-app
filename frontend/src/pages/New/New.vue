8851c5da6275de8f836949a33310fd72b71fbd29
Teh45 <theohubert3@gmx.com>
2025-01-08 11:16:26 +0000

    ✨ Add AI greeting messages: Introduce localized AI greeting messages in English and French for improved user interaction. Update the New.vue component to display the AI greeting when an agent is selected, enhancing the overall user experience in the agent selection process.

--- frontend/src/pages/New/New.vue
+++ frontend/src/pages/New/New.vue
@@ -712,6 +712,7 @@ const displayAgentSelection = async () => {
           const selectedAgentData = agents.value.find(agent => agent.id.toString() === agentId);
           if (selectedAgentData) {
             selectedAgent.value = selectedAgentData;
+            displayMessage(i18n.global.t("agent.AiGreeting"), selectedAgent.value.picture);
             setAgentLastUsed(selectedAgent.value);
           }
         });
@@ -731,6 +732,7 @@ const checkLastUsedAgent = async () => {
       console.log('Selected agent data:', selectedAgentData);
       if (selectedAgentData) {
         selectedAgent.value = selectedAgentData;
+        await displayMessage(i18n.global.t("agent.AiGreeting"), selectedAgent.value.picture);
       }
     } else {
         const aiIcon = `<path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />`;

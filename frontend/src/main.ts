95a2f7e6b65b07788dd0be1544db40205002526e
teloryfrozy <raptor.frozy@gmail.com>
2025-02-16 17:04:53 +0100

    🪲fix: issue #106

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,9 +6,11 @@ import "./assets/css/quill.css";
 import { I18n } from "vue-i18n";
 import { i18n, initializePreferences } from "./global/preferences";
 import router from "./router/router";
+import FlagIcon from "vue-flag-icon";
 
 const app = createApp(App);
 app.use(router);
+app.use(FlagIcon);
 app.use(i18n);
 
 initializePreferences(i18n as I18n).then(() => {

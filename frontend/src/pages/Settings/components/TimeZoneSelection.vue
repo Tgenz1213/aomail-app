1c30096ac60d57be2151b5b7bc4387235f681218
teloryfrozy <raptor.frozy@gmail.com>
2025-02-16 17:13:16 +0100

    🌐language: translate TimeZoneSelection

--- frontend/src/pages/Settings/components/TimeZoneSelection.vue
+++ frontend/src/pages/Settings/components/TimeZoneSelection.vue
@@ -5,14 +5,14 @@
                 <input
                     type="text"
                     v-model="searchQuery"
-                    placeholder="Search time zones..."
+                    :placeholder="i18n.global.t('settingsPage.preferencesPage.timeZoneSelection.searchPlaceholder')"
                     class="w-full p-2 border rounded-md"
                 />
             </div>
             <ListboxButton
                 class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-800 sm:text-sm sm:leading-6"
             >
-                <span class="block truncate">{{ selectedTimeZone || "Select a timezone" }}</span>
+                <span class="block truncate">{{ selectedTimeZone || i18n.global.t('settingsPage.preferencesPage.timeZoneSelection.selectPlaceholder') }}</span>
                 <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                     <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
                 </span>
@@ -30,7 +30,7 @@
                         v-if="filteredTimezones.length === 0"
                         class="relative cursor-default select-none py-2 pl-3 pr-9 text-gray-500"
                     >
-                        No results found
+                        {{ i18n.global.t('settingsPage.preferencesPage.timeZoneSelection.noResults') }}
                     </li>
                     <ListboxOption
                         v-else

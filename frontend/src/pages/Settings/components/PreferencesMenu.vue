35d01a21ebe784d1e17c2e515aa941b97cf29e95
Teh45 <theohubert3@gmx.com>
2025-02-17 22:10:12 +0000

    🎨 UI: Refine preferences menu section layout and spacing

--- frontend/src/pages/Settings/components/PreferencesMenu.vue
+++ frontend/src/pages/Settings/components/PreferencesMenu.vue
@@ -3,20 +3,21 @@
         <div class="h-full w-full flex items-center justify-center">
             <div class="flex gap-x-10 h-full w-full py-10 px-8 2xl:py-14 2xl:px-12">
                 <div class="flex-1 flex-col h-full flex-grow px-4">
-                    <div class="relative">
-                        <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                            <div class="w-full border-t border-gray-300"></div>
+                    <div class="py-4">
+                        <div class="relative">
+                            <div class="absolute inset-0 flex items-center" aria-hidden="true">
+                                <div class="w-full border-t border-gray-300"></div>
+                            </div>
+                            <div class="relative flex justify-center">
+                                <span class="bg-gray-100/70 px-4 py-1 text-md text-gray-600 rounded-full backdrop-blur-sm">
+                                    {{ $t("constants.language") }}
+                                </span>
+                            </div>
                         </div>
-                        <div class="relative flex justify-center">
-                            <span class="bg-gray-100/70 px-4 py-1 text-md text-gray-600 rounded-full backdrop-blur-sm">
-                                {{ $t("constants.language") }}
-                            </span>
+                        <div class="pt-8">
+                            <LanguageSelection />
                         </div>
                     </div>
-                    <div class="pt-8">
-                        <LanguageSelection />
-                    </div>
-
                     <div class="flex-col flex-grow w-full py-12 2xl:py-20">
                         <div class="relative">
                             <div class="absolute inset-0 flex items-center" aria-hidden="true">
@@ -33,7 +34,7 @@
                         </div>
                     </div>
 
-                    <div class="flex-col flex-grow w-full">
+                    <div class="flex-col flex-grow w-full py-4">
                         <div class="relative">
                             <div class="absolute inset-0 flex items-center" aria-hidden="true">
                                 <div class="w-full border-t border-gray-300"></div>
@@ -53,18 +54,20 @@
                 </div>
 
                 <div class="flex-1 flex-col h-full flex-grow px-4">
-                    <div class="relative">
-                        <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                            <div class="w-full border-t border-gray-300"></div>
+                    <div class="py-4">
+                        <div class="relative">
+                            <div class="absolute inset-0 flex items-center" aria-hidden="true">
+                                <div class="w-full border-t border-gray-300"></div>
+                            </div>
+                            <div class="relative flex justify-center">
+                                <span class="bg-gray-100/70 px-4 py-1 text-md text-gray-600 rounded-full backdrop-blur-sm">
+                                    {{ $t("constants.theme") }}
+                                </span>
+                            </div>
                         </div>
-                        <div class="relative flex justify-center">
-                            <span class="bg-gray-100/70 px-4 py-1 text-md text-gray-600 rounded-full backdrop-blur-sm">
-                                {{ $t("constants.theme") }}
-                            </span>
+                        <div class="pt-8">
+                            <ThemeSelection />
                         </div>
-                    </div>
-                    <div class="pt-8">
-                        <ThemeSelection />
                     </div>
 
                     <div class="flex-col flex-grow w-full py-12 2xl:py-20">

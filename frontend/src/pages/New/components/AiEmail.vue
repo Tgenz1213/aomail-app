016d8df2cec7c0577ff86f0b9dfb23b66c57affa
Teh45 <theohubert3@gmx.com>
2025-02-24 20:41:09 +0000

    🎨 Remove background class from agent icon in AiEmail component

--- frontend/src/pages/New/components/AiEmail.vue
+++ frontend/src/pages/New/components/AiEmail.vue
@@ -387,7 +387,7 @@ function askChoiceRecipier(recipients: AiRecipient[], type: string) {
     const messageHTML = `
       <div class="flex pb-6">
         <div class="mr-3 flex-shrink-0">
-          <span class="inline-flex h-12 w-12 items-center justify-center rounded-full bg-gray-900 text-white">
+          <span class="inline-flex h-12 w-12 items-center justify-center rounded-full">
             <img src="${API_BASE_URL}agent_icon/${
         selectedAgent.value.icon_name
     }" alt="Agent Icon" class="h-12 w-12 rounded-full object-cover">

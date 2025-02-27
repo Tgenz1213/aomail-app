aa53064dc475d61c5bb826ce7a636067c853d84d
teloryfrozy <raptor.frozy@gmail.com>
2025-02-27 23:51:45 +0100

    🧪tests: add Email tests

--- backend/tests/test_ai_providers_utils.py
+++ backend/tests/test_ai_providers_utils.py
@@ -1,4 +1,30 @@
+import pytest
 from aomail.ai_providers.utils import extract_json_from_response, count_corrections
+from django.contrib.auth.models import User
+from aomail.models import SocialAPI, Statistics
+from aomail.ai_providers.utils import update_tokens_stats
+
+
+@pytest.fixture
+def user():
+    return User.objects.get_or_create(username="testuser", password="testpassword")
+
+
+@pytest.fixture
+def social_api(user: User):
+    return SocialAPI.objects.get_or_create(
+        user=user,
+        email="testuser@example.com",
+        type_api="google",
+        user_description="user_description",
+        access_token="access_token",
+        refresh_token="refresh_token",
+    )
+
+
+@pytest.fixture
+def statistics(user: User):
+    return Statistics.objects.get_or_create(user=user)
 
 
 def test_extract_json_from_response():
@@ -13,6 +39,27 @@ def test_extract_json_from_response():
 
 
 def test_count_corrections():
-    assert count_corrections("Hello, world!", "Hello, world!", "Hello, world!", "Hello, world!") == 0
-    assert count_corrections("Hello, world!", "Hello, world!", "Hello world!", "Hello world!") == 2
-    assert count_corrections("Original subject", "Original body", "Corrected subject", "Corrected body") == 2
+    assert (
+        count_corrections(
+            "Hello, world!", "Hello, world!", "Hello, world!", "Hello, world!"
+        )
+        == 0
+    )
+    assert (
+        count_corrections(
+            "Hello, world!", "Hello, world!", "Hello world!", "Hello world!"
+        )
+        == 2
+    )
+    assert (
+        count_corrections(
+            "Original subject", "Original body", "Corrected subject", "Corrected body"
+        )
+        == 2
+    )
+
+
+def test_update_tokens_stats(user: User, statistics: Statistics):
+    update_tokens_stats(user, {"tokens_input": 10, "tokens_output": 20})
+    assert statistics.nb_tokens_input == 10
+    assert statistics.nb_tokens_output == 20

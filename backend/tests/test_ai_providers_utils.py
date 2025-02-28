e7c90e8117155ce5d039d54d91c2ba6ad761c677
teloryfrozy <raptor.frozy@gmail.com>
2025-02-28 10:38:03 +0100

    🧪tests: add email providers utils tests

--- backend/tests/test_ai_providers_utils.py
+++ backend/tests/test_ai_providers_utils.py
@@ -1,33 +1,8 @@
 import pytest
 from aomail.ai_providers.utils import extract_json_from_response, count_corrections
 from django.contrib.auth.models import User
-from aomail.models import SocialAPI, Statistics
+from aomail.models import Statistics
 from aomail.ai_providers.utils import update_tokens_stats
-
-
-# @pytest.fixture
-# def user():
-#     user, _ = User.objects.get_or_create(username="testuser", password="testpassword")
-#     return user
-
-
-# @pytest.fixture
-# def social_api(user: User):
-#     social_api, _ = SocialAPI.objects.get_or_create(
-#         user=user,
-#         email="testuser@example.com",
-#         type_api="google",
-#         user_description="user_description",
-#         access_token="access_token",
-#         refresh_token="refresh_token",
-#     )
-#     return social_api
-
-
-# @pytest.fixture
-# def statistics(user: User):
-#     statistics, _ = Statistics.objects.get_or_create(user=user)
-#     return statistics
 
 
 def test_extract_json_from_response():
